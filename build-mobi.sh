606757d81484ceb9c0b20e96a4561a5416db6687
Fredrik Ahlberg <fredrik@z80.se>
2015-09-27 20:12:48 +0200

    fixes

--- build-mobi.sh
+++ build-mobi.sh
@@ -3,9 +3,8 @@ mkdir -p content
 cd content
 wget -O cover.jpg http://pd-tutorial.com/KreidlerCov.jpg
 wget -r -nH --cut-dirs=1 http://pd-tutorial.com/english/
-ln -s ../pempd.opf .
-ln -s ../toc.ncx .
+cp ../pempd.opf ../toc.ncx .
 cat ../fix.css >> style.css
 cd ..
-../kindlegen/kindlegen ./content/toc.ncx ./content/pempd.opf -c2 -verbose -gif -o pempd.mobi
+./kindlegen/kindlegen ./content/toc.ncx ./content/pempd.opf -c2 -verbose -gif -o pempd.mobi
 mv ./content/pempd.mobi .
